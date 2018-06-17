40237f1dc3c00bc576557f346c1e510d13253696
Alejandro <uo251893@uniovi.es>
2018-06-17 21:37:10 +0200

    Tests arreglados

--- src/test/java/tests/IncidenceControllerTests.java
+++ src/test/java/tests/IncidenceControllerTests.java
@@ -8,8 +8,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import inciManager.incidenceController.RestUtil;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes= { RestUtil.class })
 public class IncidenceControllerTests {
 
 	@Test
